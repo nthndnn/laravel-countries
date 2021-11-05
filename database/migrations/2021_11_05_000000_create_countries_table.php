d738320e5c88f22024840598667618a67f378460
Nathan Dunn <me@ndunn.uk>
2021-11-05 20:51:37 +0000

    Add migrations

---
+++ database/migrations/2021_11_05_000000_create_countries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_official');
+            $table->string('name_common');
+            $table->string('region')->nullable();
+            $table->string('subregion')->nullable();
+            $table->string('alpha_2_code', 2)->unique();
+            $table->string('alpha_3_code', 3)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
