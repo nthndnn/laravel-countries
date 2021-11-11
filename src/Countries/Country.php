aa58ea0c74540c5150e1c824fd9bc7738d37cb6e
Nathan Dunn <me@ndunn.uk>
2021-11-11 00:22:02 +0000

    Add continent Country model

--- src/Countries/Country.php
+++ src/Countries/Country.php
@@ -4,7 +4,9 @@ namespace NathanDunn\Countries\Countries;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use NathanDunn\Countries\Continents\Continent;
 use NathanDunn\Countries\Currencies\Currency;
 
 class Country extends Model
@@ -12,6 +14,11 @@ class Country extends Model
     use HasFactory;
 
     protected $table = 'countries';
+
+    public function continent(): BelongsTo
+    {
+        return $this->belongsTo(Continent::class);
+    }
 
     public function currencies(): BelongsToMany
     {
