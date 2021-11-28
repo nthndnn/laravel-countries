876bf3874a59e4fefc373ceffa75a886c808f09b
Nathan Dunn <me@ndunn.uk>
2021-11-28 23:38:14 +0000

    Add SyncCountryTest

---
+++ tests/Unit/Countries/Jobs/SyncCountryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace NathanDunn\Countries\Tests\Unit\Countries\Jobs;
+
+use Illuminate\Bus\Dispatcher;
+use Illuminate\Support\Arr;
+use NathanDunn\Countries\Continents\Continent;
+use NathanDunn\Countries\Continents\ContinentRepository;
+use NathanDunn\Countries\Countries\CountryRepository;
+use NathanDunn\Countries\Countries\Jobs\CreateCountry;
+use NathanDunn\Countries\Countries\Jobs\SyncCountry;
+use NathanDunn\Countries\Currencies\CurrencyRepository;
+use NathanDunn\Countries\Tests\TestCase;
+
+class SyncCountryTest extends TestCase
+{
+    /** @test */
+    public function can_sync_country()
+    {
+        $data = $this->getCountries();
+        $countryData = Arr::first($data);
+        /** @var CountryRepository $countryRepository */
+        $countryRepository = app(CountryRepository::class);
+        /** @var Dispatcher $dispatcher */
+        $dispatcher = app(Dispatcher::class);
+
+        Continent::factory()->create(['name' => 'Asia']);
+
+        (new SyncCountry($countryData))
+            ->handle($countryRepository, $dispatcher);
+
+        $this->assertDatabaseHas('countries', ['alpha_3_code' => Arr::get($countryData, 'cca3')]);
+    }
+}
