3598913a979670186119f853042e4f551059a33d
Nathan Dunn <me@ndunn.uk>
2021-11-29 10:01:40 +0000

    Add SyncCountriesTest

---
+++ tests/Commands/SyncCountriesTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace NathanDunn\Countries\Tests\Countries\Jobs;
+
+use Illuminate\Support\Facades\Http;
+use NathanDunn\Countries\Tests\TestCase;
+
+class SyncCountriesTest extends TestCase
+{
+    /** @test */
+    public function can_sync_country()
+    {
+        $countries = $this->getCountries();
+        Http::fake([
+            'restcountries.com/v3.1/all' => Http::response($countries, 200, ['Content-Type' => 'application/json']),
+        ]);
+
+        $this->artisan('countries:sync')->assertExitCode(0);
+    }
+}
